928c3c03536d329a8b748859cb397c51e115f941
Nick DeJesus <nickdejesus@Nicks-MacBook-Pro.local>
2017-12-18 22:15:00 -0500

    setting start and end points for slider referring to YT vid

--- src/components/GifCuttIngForm/index.js
+++ src/components/GifCuttIngForm/index.js
@@ -3,6 +3,7 @@ import YouTube from 'react-youtube';
 import Slider from 'rc-slider';
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
+//import Slider from 'react-rangeslider'
 
 const youtubeParse = (url) => {
     const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
@@ -15,7 +16,9 @@ class GifCuttingForm extends Component {
         super();
         this.state = {
             url: "",
-            videoDuration: 0
+            videoDuration: 0,
+            time: [0,0],
+            currentVidEvent: null
         }
     }
 
@@ -29,6 +32,9 @@ class GifCuttingForm extends Component {
     }
 
     onReady(event, url) {
+        this.setState({
+            currentVidEvent: event.target
+        })
         if(event.target.getDuration() === 0) {
             event.target.playVideo()
         } else {
@@ -49,22 +55,58 @@ class GifCuttingForm extends Component {
         duration = Math.round(duration)
         return (
             <Range
-              allowCross={false}
+            allowCross={false}
+              value={this.state.time}
               max={duration}
+              onChange={(value) => this.onSliderChange(value)}
               tipProps={{
                   placement: 'top',
                   prefixCls: 'rc-slider-tooltip',
-              }}
+                }}
               tipFormatter={duration => this.secondsToMinutes(duration)}
             />
         )
     }
 
-    render() {
-        this.state.videoDuration === 0 ? console.log('0 is true') : ('not 0')
-        const opts = {
-            width: '500',
+    onSliderChange(value) {
+        const initialStart = this.state.time[0];
+        const initialEnd = this.state.time[1];
+
+        if(value[0] === initialStart) {
+            console.log('grabbing end handle')
+            this.state.currentVidEvent.seekTo(value[1])
+            this.setState({
+                time: value
+            })
         }
+
+        if(value[1] === initialEnd) {
+            console.log('grabbing start handle')
+            this.state.currentVidEvent.seekTo(value[0])
+            this.setState({
+                time: [value[0], initialEnd]
+            })
+        }
+    }
+
+    sliderUpdate(event, flag) {
+        if(flag === 'play') {
+            console.log('play', Math.round(event.target.getCurrentTime()), this.state.time)
+            this.setState({
+                time: [Math.round(event.target.getCurrentTime()), this.state.time[1]]
+            })
+        }
+
+        if (flag === 'pause') {
+            console.log('pause', Math.round(event.target.getCurrentTime()), this.state.time)
+            this.setState({
+                time: [this.state.time[0], Math.round(event.target.getCurrentTime())]
+            })
+        }
+    }
+
+    render() {
+        const opts = { width: '500', }
         return (
             <div className="gif-cutting-form-container">
                 <h2>Paste YouTube link below</h2>
@@ -72,6 +114,8 @@ class GifCuttingForm extends Component {
                     opts={opts}
                     videoId={this.state.url}
                     onStateChange={(event) => this.onReady(event, this.state.url)}
+                    onPlay={(event) => this.sliderUpdate(event, 'play')}
+                    onPause={(event) => this.sliderUpdate(event, 'pause')}
                 />
                 <form onSubmit={this.props.handleGifCutter} noValidate>
                     <label>
